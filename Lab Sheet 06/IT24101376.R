78eb49addfbd2cf4eb79cd7a195954b4aedbe582
IT24101376 <IT24101376@my.sliit.lk>
2025-09-15 18:40:59 +0530

    IT24101376_lab6

---
+++ Lab Sheet 06/IT24101376.R
@@ -0,0 +1,22 @@
+#Exercise
+
+setwd("C:\\Users\\ASUS TUF\\Desktop\\IT24101376_Lab6")
+getwd()
+#01 
+
+#i
+#Binomial Distribution
+#n=50  , p = 0.85
+
+#ii
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#02
+#i
+# number of calls received in one hour
+
+#ii
+#Poisson Distribution
+
+#iii
+dpois(15, 12)
