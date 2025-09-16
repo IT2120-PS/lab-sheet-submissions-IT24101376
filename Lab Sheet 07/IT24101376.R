5cf0db914501e611a98a742cc23694f0e4024f61
IT24101376 <IT24101376@my.sliit.lk>
2025-09-17 01:09:18 +0530

    IT24101376_Lab07

---
+++ Lab Sheet 07/IT24101376.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\ASUS TUF\\Desktop\\IT24101376_Lab07")
+getwd()
+
+#01
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#02
+pexp(2, rate = 1/3)
+
+
+#03
+#i
+1 - pnorm(130, 100, 15)
+
+#ii
+qnorm(0.95, 100, 15)
+
